b7a6606536a735d4012135dbbb2aed4b85285ed9
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-01-30 12:41:14 +0100

    using the commandLine to select the mode data and the key

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -1,42 +1,49 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String function = scanner.nextLine();
-        String message = scanner.nextLine();
-        int key = scanner.nextInt();
-        if (function.equals("enc")) {
-            System.out.println(Cipher.EncrypterByShifting(message, key));
-        } else if (function.equals("dec")) {
-            System.out.println(Cipher.DecrypterByShifting(message, key));
-        }
-
-    }
-}
-class Cipher {
-    
     /**
      * encrypting by shifting using a key
+     *
      * @param inputText
      * @return
      */
     public static String EncrypterByShifting(String inputText, int key) {
         char[] listOfChars = new char[inputText.length()];
-        for (int i = 0; i < inputText.length(); i ++) {
-                    int step = inputText.charAt(i) + key;
-                    listOfChars[i] = (char) step;
-                }
+        for (int i = 0; i < inputText.length(); i++) {
+            int step = inputText.charAt(i) + key;
+            listOfChars[i] = (char) step;
+        }
         return String.valueOf(listOfChars);
     }
+
     public static String DecrypterByShifting(String inputText, int key) {
         char[] listOfChars = new char[inputText.length()];
-        for (int i = 0; i < inputText.length(); i ++) {
+        for (int i = 0; i < inputText.length(); i++) {
             int step = inputText.charAt(i) - key;
             listOfChars[i] = (char) step;
         }
         return String.valueOf(listOfChars);
     }
+
+    public static void main(String[] args) {
+        String mode = "enc";
+        String data = "";
+        int key = 0;
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-mode")) {
+                mode = args[i + 1];
+            } else if (args[i].equals("-key")) {
+                key = Integer.parseInt(args[i + 1]);
+            } else if (args[i].equals("-data")) {
+                data = args[i + 1];
+            }
+        }
+            if (mode.equals("enc")) {
+                System.out.println(EncrypterByShifting(data, key));
+            } else if (mode.equals("dec")) {
+                System.out.println(DecrypterByShifting(data, key));
+            }
+
+        }
 }
