7a0826404bdaf7f15d2287e1c49f78a01288a087
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-01-30 11:11:26 +0100

    encrypting by shifting using a key

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -6,12 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String inputText = scanner.nextLine();
+        int key = scanner.nextInt();
         inputText = inputText.toLowerCase();
-        System.out.println(Cipher.Converter(inputText));
+        System.out.println(Cipher.ShiferEncrypter(inputText, key));
     }
 }
 class Cipher {
-    public static String Converter(String inputText) {
+
+    /**
+     * Encryption by reflection of alphabets: a to z and z to a...
+     * @param inputText
+     * @return
+     */
+    public static String Encrypter(String inputText) {
         char[] listOfChars = new char[inputText.length()];
         String regex = "[a-z]";
         String str = "";
@@ -21,6 +28,36 @@ class Cipher {
                 int step = inputText.charAt(i) - 97;
                 int var = 122 - step;
                 listOfChars[i] = (char) var;
+                str = "";
+            } else {
+                listOfChars[i] = inputText.charAt(i);
+                str = "";
+            }
+        }
+
+        return String.valueOf(listOfChars);
+    }
+
+    /**
+     * encrypting by shifting using a key
+     * @param inputText
+     * @return
+     */
+    public static String ShiferEncrypter(String inputText, int key) {
+        char[] listOfChars = new char[inputText.length()];
+        String regex = "[a-z]";
+        String str = "";
+        for (int i = 0; i < inputText.length(); i ++) {
+            str += inputText.charAt(i);
+            if (str.matches(regex)) {
+                if (inputText.charAt(i) + key <= 122) {
+                    int step = inputText.charAt(i) + key;
+                    listOfChars[i] = (char) step;
+                } else {
+                    int step1 = inputText.charAt(i) + key - 122;
+                    int step2 = 97 + step1 - 1;
+                    listOfChars[i] = (char) step2;
+                }
                 str = "";
             } else {
                 listOfChars[i] = inputText.charAt(i);
